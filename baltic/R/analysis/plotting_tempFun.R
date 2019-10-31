e620c01757846adf4349d1f745ed14d2691f208b
Max Lindmark <maxo.lindmark@gmail.com>
2019-10-31 16:41:15 +0100

    Will eventually use this script to visualise and exemplify the tempFun and the scalars

---
+++ baltic/R/analysis/plotting_tempFun.R
@@ -0,0 +1,26 @@
+tempFun <- function(temperature, t_ref, Ea, c_a, w) # default are 0 for now as deactivation is buggy
+{
+  # tempFun returns a matrix with w (size) as columns and temperature as rows
+  
+  # equation
+  # (w^(c_a*(temperature-t_ref)))  *exp((-Ea/8.617332e-5)*((1/temperature) - (1/t_ref)))
+  # *(1/(w^(c_d*(temperature-t_d)))*exp((-Ed/8.617332e-5)*((1/temperature) - (1/t_d))))
+  
+  temperature <- temperature + 273 # converting to Kelvin from Celcius
+  t_ref <- t_ref + 273
+  
+  temperatureScalar <- t(sapply(w,FUN = function(x){x^(c_a*(temperature-(t_ref)))}) *exp((-Ea/8.617332e-5)*((1/temperature) - (1/(t_ref))))) 
+  
+  return(temperatureScalar)
+}
+
+tempFun(temperature = c(5:15), t_ref = 10, Ea = 0, c_a = 0, w = c(1:10))
+
+tempFun(temperature = c(5:15), t_ref = 10, Ea = 0.63, c_a = 0, w = c(1:10))
+tempFun(temperature = c(5:15), t_ref = 10, Ea = 0.63, c_a = 0, w = c(1))
+
+tempFun(temperature = c(5:15), t_ref = 10, Ea = 0.63, c_a = -0.02, w = c(1:10))
+
+
+
+
