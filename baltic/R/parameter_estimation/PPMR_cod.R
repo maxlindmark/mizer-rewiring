8eb59ed40a0bdd65c200579c52bb47ef7e44aa2f
Max Lindmark <maxo.lindmark@gmail.com>
2019-11-07 17:16:30 +0100

    Clean up PPMR script, save supp fig on PPMR

--- baltic/R/parameter_estimation/PPMR_cod.R
+++ baltic/R/parameter_estimation/PPMR_cod.R
@@ -13,10 +13,6 @@
 #
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-# Questions in progress marked with ***
-
-# NOT UPDATE, MAYBE WILL NOT USE
-
 #======== A. LOAD LIBRARIES AND READ DATA ========
 rm(list = ls())
 
@@ -35,12 +31,12 @@ library(patchwork) # v0.0.1
 
 # Stomach data: http://ecosystemdata.ices.dk/stomachdata/download.aspx 
 #  (filter only by cod, Gadus morhua)
-
-dat <- read.csv("data/PPMR/cod/StomachDataSet201932090580.csv", sep = ",")
+dat <- read.csv("baltic/data/PPMR/cod/StomachDataSet201932090580.csv", sep = ",")
 
 head(dat)
 str(dat)
 
+# Filter data that covers the calibration period
 min_cal_yr <- 1992
 max_cal_yr <- 2002
 
@@ -85,9 +81,9 @@ dat %>%
   theme_bw(base_size = 18) +
   NULL
 
-# - Ok, this means that for year prior to 2002, prey weight is not individual 
-#  weight but per prey species. So when there is n prey, I need to divide
-#  the weight by n prey
+# Ok, this means that for year prior to 2002, prey weight is not individual 
+# weight but per prey species. So when there is n prey, I need to divide
+# the weight by n prey
 
 # Filter spatial distribution
 # Split AreaCode-factor to more easily split by ICES sub division
@@ -123,8 +119,8 @@ ggplot(dat, aes(Code_s, fill = factor(Area_s))) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 dat <- dat %>% filter(calib_yr == "Y" & 
-                        Prey_DigestionStage == "0" & 
-                          Stomach_Empty == 0)
+                      Prey_DigestionStage == "0" & 
+                      Stomach_Empty == 0)
 
 # First need to filter stomachs with 0 "Prey_TotalNo" but with a weight, because 
 # that must be an error. Because if it's also before 2000, I need to now the # of
@@ -153,7 +149,7 @@ ggplot(dat, aes(year, fill = factor(Prey_TotalNo))) +
 # Calculate n prey by predator, plot summary
 dat_sum <- dat %>% 
   group_by(ICES_ItemID) %>% 
-  mutate(n = n())
+  dplyr::mutate(n = n())
 
 ggplot(dat_sum, aes(factor(n))) +
   geom_bar() +
@@ -171,7 +167,7 @@ ggplot(dat_sum, aes(factor(RecordType), fill = factor(year))) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-# Ok, I'm dividing prey weight by Prey_TotalNo if older than 2000
+# Divide prey weight by Prey_TotalNo if older than 2000
 dat_sum$ind_weight <- ifelse(dat_sum$year < 2000, 
                              dat_sum$Prey_Weight / dat_sum$Prey_TotalNo,
                              dat_sum$Prey_Weight)
@@ -190,76 +186,14 @@ head(data.frame(dat_sum), 25)
 
 # Calculate average weight of prey within each individual (to avoid pseudoreplication)
 dat_agg <- data.frame(dat_sum %>% 
-  group_by(ICES_ItemID) %>% 
-  mutate(mean_prey_w = mean(ind_weight)))
+  dplyr::group_by(ICES_ItemID) %>% 
+  dplyr::mutate(mean_prey_w = mean(ind_weight)))
 
 # Ok, looks like there's no weight in the data, need to convert from length!
 dat_agg$pred_weight <- 0.01*dat_agg$Predator_Lengh.mean.^3
 
-# Fit log()-log() and plot (slope is PPMR)
-m_log <- lm(log10(dat_agg$pred_weight) ~ log10(dat_agg$mean_prey_w))
-summary(m_log)
-
-#https://stats.stackexchange.com/questions/18480/interpretation-of-log-transformed-predictor
-
-ggplot(dat_agg, aes(log10(mean_prey_w), log10(pred_weight))) +
-  geom_point(size = 3) +
-  geom_abline(slope = 0.20559, intercept = 2.55558, color = "red", size = 1) +
-  theme_bw(base_size = 18) +
-  #annotate("text", label = "PPMR = 1.61", x = 0, 
-           y = 1.5, size = 10, col = "blue") +
-  NULL
-
-
-# Fit normal scale and plot
-m <- lm(dat_agg$pred_weight ~ dat_agg$mean_prey_w)
-summary(m)
-
-ggplot(dat_agg, aes(mean_prey_w, pred_weight)) +
-  geom_point(size = 3) +
-  geom_abline(slope = 36.605, intercept = 451.248, color = "red", size = 1) +
-  theme_bw(base_size = 18) +
-  annotate("text", label = "PPMR = 36.605", x = 75, y = 6000, size = 10, col = "blue") +
-  NULL
-
 # Calculate PPMR
 dat_agg$PPMR <- dat_agg$pred_weight / dat_agg$mean_prey_w
-
-# Plot PPMR
-cc <- dat_agg %>% 
-  distinct(ICES_ItemID, .keep_all = TRUE) %>% 
-  ggplot(., aes(PPMR)) +
-  geom_histogram(size = 3) +
-  ggtitle("C") +
-  theme_bw(base_size = 18)
-
-a <- dat_agg %>% 
-  distinct(ICES_ItemID, .keep_all = TRUE) %>% 
-  ggplot(., aes(log10(PPMR))) +
-  geom_histogram(size = 3) +
-  theme_bw(base_size = 18) +
-  ggtitle("A") +
-  annotate("text", label = "mean PPMR=426\nsd PPMR=5.63", 
-           x = 4, y = 110, size = 4.5, col = "blue") +
-  NULL
-
-d <- dat_agg %>% 
-  distinct(ICES_ItemID, .keep_all = TRUE) %>% 
-  filter(PPMR > 0) %>% 
-  ggplot(., aes(x = "", y = PPMR)) +
-  geom_boxplot(size = 2) +
-  ggtitle("D") +
-  2.630193
-  theme_bw(base_size = 18)
-
-b <- dat_agg %>% 
-  distinct(ICES_ItemID, .keep_all = TRUE) %>% 
-  ggplot(., aes(x = "", y = log10(PPMR))) +
-  geom_boxplot(size = 2) +
-  geom_jitter(alpha = 0.5, height = 0, width = 0.2) +
-  theme_bw(base_size = 18) +
-  ggtitle("B") +
-  NULL
 
 # Calculate log mean and standard deviation
 dat_agg %>%
@@ -269,41 +203,20 @@ dat_agg %>%
             mean_n = 10^(mean(log10(PPMR))),
             sd_n   = 10^(sd(log10(PPMR))))
 
-a + b
-
-# Unlogged 
-dat_agg %>%
+# Plot log10(PPMR)
+dat_agg %>% 
   distinct(ICES_ItemID, .keep_all = TRUE) %>% 
-  summarize(mean = mean(PPMR),
-            sd   = sd(PPMR))
+  ggplot(., aes(log10(PPMR))) +
+  geom_histogram(size = 3) +
+  theme_classic(base_size = 18) +
+  geom_vline(xintercept = 2.63, color = "red", linetype = 2, size = 1) +
+  coord_cartesian(expand = 0) +
+  annotate("text", label = "mean PPMR=426\nsd PPMR=5.63", 
+           x = Inf, y = Inf, size = 4.5, col = "blue",
+           vjust = 1, hjust = 1) +
+  NULL
 
-## SUMMARY ##
-(a + b) / (cc + d)
-
-
-# Conclusion: Read https://ac.els-cdn.com/B9780123864758000071/1-s2.0-B9780123864758000071-main.pdf?_tid=5ab9d980-ec6e-486e-9ce4-eaedfd604d4f&acdnat=1553088380_5112376b192e3f6356bccf11358b5b66
-
-# Most references (Reum 2018 and refs therein) calculate PPMR either by species or by individual,
-# and then showing that in e.g histogram or boxplot
-# Another option is to plot and take slope
-
-# WHEN TO LOG? WHICH METHOD?
-
-
-
-
-
-
-
-
-# individual link PPMR can yield high PPMR. Means of all those PPMR's will be super large, whereas in a scatter plot, those might be outliers at best-
-
-
-
-
-
-
-
+ggsave("baltic/figures/supp/PPMR.pdf", plot = last_plot(), width = 15, height = 15, units = "cm")
 
 
 
