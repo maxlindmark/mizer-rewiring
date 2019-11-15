2dde6a8313abb878b5a6af223a00498abbc554e4
maxlindmark <maxo.lindmark@gmail.com>
2019-11-15 10:43:31 +0100

    fix typo

--- baltic/R/functions/getMortality.R
+++ baltic/R/functions/getMortality.R
@@ -1,5 +1,5 @@
 # Create getMortality function..
-getSpectra <- function(object){
+getMortality <- function(object){
 m2_time <- getM2(object, time_range = dim(object@effort)[1], intakeScalar = object@intTempScalar[,,dim(object@effort)[1]], drop = FALSE)
 m2 <- apply(m2_time, c(2, 3), mean)
 m2 <- m2[as.character(dimnames(m2)[[1]]) %in% object@params@species_params$species, , 
