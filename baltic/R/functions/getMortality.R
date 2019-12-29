c9bc2a36657e5f11b6a0888340cb7d580da88781
maxlindmark <maxo.lindmark@gmail.com>
2019-12-29 17:43:36 +0100

    test mort

--- baltic/R/functions/getMortality.R
+++ baltic/R/functions/getMortality.R
@@ -1,6 +1,6 @@
 # Create getMortality function..
 getMortality <- function(object){
-m2_time <- getZ(object, time_range = dim(object@effort)[1], intakeScalar = object@intTempScalar[,,dim(object@effort)[1]], drop = FALSE)
+m2_time <- getPredMort(object, time_range = dim(object@effort)[1], intakeScalar = object@intTempScalar[,,dim(object@effort)[1]], drop = FALSE)
 m2 <- apply(m2_time, c(2, 3), mean)
 m2 <- m2[as.character(dimnames(m2)[[1]]) %in% object@params@species_params$species, , 
          drop = FALSE]
