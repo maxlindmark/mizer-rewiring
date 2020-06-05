f0e0774427da59a8ba9310ecba182fcce426c3c3
Max Lindmark <maxlindmark@Maxs-MacBook-Pro.local>
2020-06-05 12:13:45 +0200

    update to include get_time_elements so that it reads in the global environment

--- baltic/R/functions/getSpectra.R
+++ baltic/R/functions/getSpectra.R
@@ -1,3 +1,19 @@
+# Read in get_time_elements in the global environment, can't get it to work from project_methods
+get_time_elements <- function(sim, time_range, slot_name = "n"){
+  if (!is(sim, "MizerSim"))
+    stop("First argument to get_time_elements function must be of class MizerSim")
+  time_range <- range(as.numeric(time_range))
+  # Check that time range is even in object
+  sim_times <- as.numeric(dimnames(sim@effort)$time)
+  sim_time_range <- range(sim_times)
+  if ( (time_range[1] < sim_time_range[1]) |
+       (time_range[2] > sim_time_range[2]))
+    stop("Time range is outside the time range of the model")
+  time_elements <- (sim_times >= time_range[1]) & (sim_times <= time_range[2])
+  names(time_elements) <- dimnames(sim@effort)$time
+  return(time_elements)
+}
+
 # Create getSpectra function..
 getSpectra <- function(object){
   time_range  <- max(as.numeric(dimnames(object@n)$time))
